e0d208789cf9e966370667341d938f1c2778e2f8
Veselin Stefanov <veski4a92@gmail.com>
2020-09-15 07:10:55 +0300

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Veselin Stefanov on 15.09.20.
+//  Copyright © 2020 Veselin Stefanov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

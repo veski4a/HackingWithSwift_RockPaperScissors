b8a5c13796d9e6e1663cb69f94727f2ead6f28f5
Veselin Stefanov <veski4a92@gmail.com>
2020-09-16 07:28:03 +0300

    Round reset after 10 straight rounds

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -9,15 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    private enum Moves: Int{
+        case Rock
+        case Paper
+        case Scissors
+    }
+    
     private let moves = ["Rock", "Paper", "Scissors"]
     
     @State private var appMove = Int.random(in: 0 ..< 3)
     @State private var playerShouldWin = Bool.random()
+    @State private var playerGuessedRight = false
+    
+    @State private var playerScore = 0
+    @State private var roundNumber = 1
+    
     @State private var showWinLooseAlert = false
     @State private var showScoreAlert = false
-    @State private var roundNumber = 1
-    @State private var playerGuessedRight = false
-    @State private var playerScore = 0
     
     var body: some View {
         VStack{
@@ -39,59 +47,62 @@ struct ContentView: View {
                 Button(action: {
                     self.onMoveTap(whichMove: number)
                 }){
-                    if number == self.appMove{
-                        Image(self.moves[number])
-                            .renderingMode(.original)
-                            .shadow(color: Color.red, radius: 3)
-                    }
-                    else{
-                        Image(self.moves[number])
-                            .renderingMode(.original)
-                    }
+                    
+//                     If the app shows what move has been chosen
+                     
+                     if number == self.appMove{
+                         Image(self.moves[number])
+                             .renderingMode(.original)
+                             .shadow(color: Color.red, radius: 3)
+                     }
+                     else{
+                         Image(self.moves[number])
+                             .renderingMode(.original)
+                     }
+                     
+                     
+                    
+//                    Image(self.moves[number])
+//                        .renderingMode(.original)
                 }
             }
             
             Spacer()
             
             .alert(isPresented: $showWinLooseAlert){
-                Alert(title: Text("Round ended"), message: playerGuessedRight ? Text("You are correct") : Text("Wrong, try again!"), dismissButton: .default(Text("Continue")){
+                Alert(title: playerGuessedRight ? Text("You are correct") : Text("Wrong, try again!"), message: Text("Your score \(self.playerScore)"), dismissButton: .default(Text("Continue")){
                         self.resetRound()
                     })
             }
         }
     }
     
-    private func onMoveTap(whichMove playerMove: Int){
-        switch(appMove){
-        case 0:
-            if playerShouldWin{
-                self.playerGuessedRight = playerMove == 1
-            }
-            else{
-                self.playerGuessedRight = playerMove == 2
-            }
-        case 1:
-            if playerShouldWin{
-                self.playerGuessedRight = playerMove == 2
-            }
-            else{
-                self.playerGuessedRight = playerMove == 0
-            }
-        case 2:
-            if playerShouldWin{
-                self.playerGuessedRight = playerMove == 0
-            }
-            else{
-                self.playerGuessedRight = playerMove == 1
-            }
-        default:
-            self.playerGuessedRight = false
+    private func hasPlayerWon(appMove: Int, playerMove: Int) -> Bool{
+        let appMove = Moves(rawValue: self.appMove)
+        let playerMove = Moves(rawValue: playerMove)
+        
+        if appMove == Moves.Rock && playerMove != Moves.Paper{
+            return false
         }
         
+        if appMove == Moves.Paper && playerMove != Moves.Scissors{
+            return false
+        }
+        
+        if appMove == Moves.Scissors && playerMove != Moves.Rock{
+            return false
+        }
+        
+        return true
+    }
+    
+    private func onMoveTap(whichMove playerMove: Int){
+        
+        self.playerGuessedRight = hasPlayerWon(appMove: self.appMove, playerMove: playerMove) == playerShouldWin
         if self.playerGuessedRight{
             self.playerScore += 1
         }
-        else{
+        else if self.playerScore > 0{
             self.playerScore -= 1
         }
         
@@ -100,7 +111,15 @@ struct ContentView: View {
     
     private func resetRound(){
         
-        self.roundNumber += 1
+        // If the last round is reached
+        if roundNumber == 10{
+            roundNumber = 1
+            playerScore = 0
+        }
+        else{
+            roundNumber += 1
+        }
+        
         self.appMove = Int.random(in: 0 ..< 3)
         self.playerShouldWin = Bool.random()
         self.playerGuessedRight = false
